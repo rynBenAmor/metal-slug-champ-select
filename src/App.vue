cb68dce24d04f18bda58b2ef7609e0f216193c3b
Rayen <153410746+rynBenAmor@users.noreply.github.com>
2025-08-21 20:05:31 +0100

    2nd commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
+
   <div class="crt-effect">
     <div class="scanlines"></div>
     <div class="container">
       <header>
         <h1>METAL SLUG X</h1>
         <div class="player-info">
-          <input v-if="!playerName" v-model="playerTagInput" @keyup.enter="setPlayerName"
+          <input ref="refNameInput" v-if="!playerName" v-model="playerTagInput" @keyup.enter="setPlayerName"
             placeholder="ENTER PLAYER NAME" type="text" maxlength="12">
           <button type="button" v-if="!playerName" @click="setPlayerName">INSERT COIN</button>
           <div v-if="playerName" class="player-tag">
@@ -55,14 +56,11 @@
     </div>
   </div>
 
-
-
-
 </template>
 
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import data from '@/data/characters.json'
 
 
@@ -71,6 +69,13 @@ const playerName = ref("");
 const selectedCharacterId = ref(null);
 
 const characters = ref(data);
+
+const refNameInput = ref(null)
+
+onMounted(()=>{
+  refNameInput.value.focus()
+})
+
 
 const setPlayerName = () => {
   if (playerTagInput.value.trim()) {
